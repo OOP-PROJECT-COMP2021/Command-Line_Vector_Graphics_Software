63ceb14bcc05381f73ea4592ae868e79f0753dc5
Shanpoo0v0 <cdrjerry0417@gmail.com>
2021-10-16 00:05:44 +0800

    boundingbox function
    add the structure of boundingbox function

---
+++ ClevisProjectGroup1/src/hk/edu/polyu/comp/comp2021/clevis/model/BoundingBox.java
@@ -0,0 +1,11 @@
+package hk.edu.polyu.comp.comp2021.clevis.model;
+class BoundingBox extends Rectangle {
+
+    BoundingBox(Shape inShape) {
+        super("bounded" + inShape.getName(), inShape.getLeftBounding(), inShape.getTopBounding(), inShape.getRightBounding() - inShape.getLeftBounding(), inShape.getTopBounding() - inShape.getBottomBounding());
+    }
+
+    public String listInfo(){
+        return "[BoundingBox]: Name: "+getName()+"; Top-left corner:"+getTopLeftCorner().getX()+","+getTopLeftCorner().getX()+"; Width, Height:"+getWidth()+","+getHeight();
+    }
+}
--- SampleProject/src/hk/edu/polyu/comp/comp2021/clitoris/Application.java
+++ SampleProject/src/hk/edu/polyu/comp/comp2021/clitoris/Application.java
@@ -1,11 +1,17 @@
-//package hk.edu.polyu.comp.comp2021.clitoris;
-//
-//import hk.edu.polyu.comp.comp2021.clitoris.model.Clitoris;
-//
-//public class Application {
-//
-//    public static void main(String[] args){
-//        Clitoris clitoris = new Clitoris();
-//        // Initialize and utilize the system
-//    }
-//}
+package hk.edu.polyu.comp.comp2021.clevis;
+
+import hk.edu.polyu.comp.comp2021.clevis.model.Clevis;
+
+public class Application {
+
+    public static void main(String[] args){
+        Clevis clevis = new Clevis();
+        Scanner scanner = new Scanner(System.in);
+        TextIO textIO = TextIOFactory.getTextIO();
+
+        String
+        // Initialize and utilize the system
+    }
+
+
+}
