8fabe39194053f6a2b4609faf8a9d0b1765c4f08
Shanpoo0v0 <cdrjerry0417@gmail.com>
2021-11-06 18:00:20 +0800

    Revert "delete sample record to test create new record"
    This reverts commit 0fe3727e0300659ee3c1cd6ee21f0e8cf1aeb78e.

--- ClevisProjectGroup1/src/hk/edu/polyu/comp/comp2021/clevis/view/circleDialog.java
+++ ClevisProjectGroup1/src/hk/edu/polyu/comp/comp2021/clevis/view/circleDialog.java
@@ -8,14 +8,13 @@ import java.awt.event.ActionListener;
 public class circleDialog extends JDialog {
     private final String OVAL_SHAPE = "oval";
     private String shape = "";
-    private JTextField locationX, locationY, radius;
+    private JTextField locationX,locationY,radius;
 
-    public void setY(int i) {
+    public void setY(int i){
         locationY.setText(locationX.getText());
     }
-
     //弹窗
-     public circleDialog() {
+    public circleDialog() {
         JFrame dialog = new JFrame("DrawCircle弹窗");
         dialog.setVisible(true);
         dialog.setBounds(100, 100, 500, 500);
@@ -35,11 +34,11 @@ public class circleDialog extends JDialog {
         labelLocationY.setText("LocationY:");
         labelRadius.setText("Radius:");
 
-        labelCircle.setBounds(0, 300, 150, 50);
-        labelName.setBounds(0, 350, 50, 30);
-        labelLocationX.setBounds(0, 380, 80, 30);
-        labelLocationY.setBounds(0, 410, 80, 30);
-        labelRadius.setBounds(0, 440, 80, 30);
+        labelCircle.setBounds(0,300,150,50);
+        labelName.setBounds(0,350,50,30);
+        labelLocationX.setBounds(0,380,80,30);
+        labelLocationY.setBounds(0,410,80,30);
+        labelRadius.setBounds(0,440,80,30);
 
         dialog.add(labelCircle);
         dialog.add(labelName);
@@ -53,10 +52,10 @@ public class circleDialog extends JDialog {
         locationY = new JTextField("");
         radius = new JTextField("");
         // 设置输入文本框的位置信息
-        name.setBounds(80, 350, 180, 30);
-        locationX.setBounds(80, 380, 180, 30);
-        locationY.setBounds(80, 410, 180, 30);
-        radius.setBounds(80, 440, 180, 30);
+        name.setBounds(80,350,180,30);
+        locationX.setBounds(80,380,180,30);
+        locationY.setBounds(80,410,180,30);
+        radius.setBounds(80,440,180,30);
         // 添加输入文本框
         dialog.getContentPane().add(name);
         dialog.getContentPane().add(locationX);
@@ -66,60 +65,43 @@ public class circleDialog extends JDialog {
         // 添加按钮back及其响应事件
         JButton backButton = new JButton("Back");
         dialog.add(backButton);
-        backButton.setBounds(400, 425, 100, 25);
+        backButton.setBounds(400, 425, 100,25);
         backButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                new drawDialog();
-            }
+            public void actionPerformed(ActionEvent e) {new drawDialog();}
         });
 
         //创建画布对象
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(300,300));
-        panel.setVisible(true);
-        panel.setBounds(0,0,400,400);
+        MyCanvas drawArea = new MyCanvas();
+        // 绘图区域
+        //drawArea.setPreferredSize(new Dimension(300,300));
+        drawArea.setVisible(true);
+        drawArea.setBounds(0,0,400,400);// 必须设置大小及坐标，因为当前layout为null
+
+        // 创建画圆按钮
         JButton circleButton = new JButton("drawCircle");
         circleButton.setBounds(400, 400, 100, 25);
-        frame.add(circleButton);
+        dialog.add(circleButton);
         circleButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                //setY(Integer.parseInt(locationX.getText()));
+                shape = OVAL_SHAPE;
+                drawArea.repaint();
             }
-        // 绘图区域
-        //drawArea.setPreferredSize(new Dimension(300,300));
-//        drawArea.setVisible(true);
-//        drawArea.setBounds(0,0,400,400);// 必须设置大小及坐标，因为当前layout为null
-//
-//        // 创建画圆按钮
-//        JButton circleButton = new JButton("drawCircle");
-//        circleButton.setBounds(400, 400, 100, 25);
-//        dialog.add(circleButton);
-//        circleButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                //setY(Integer.parseInt(locationX.getText()));
-//                shape = OVAL_SHAPE;
-//                Component background = dialog.getContentPane().add(drawArea);
-//                MyCanvas drawArea1 = new MyCanvas();
-//                background.add()
-//
-//            }
-//        });
-//        //dialog.setLayout(null);?????加上之后不显示？？？？
-//        dialog.getContentPane().add(drawArea);
-//    }
+        });
+        //dialog.setLayout(null);?????加上之后不显示？？？？
+        dialog.getContentPane().add(drawArea);
+    }
 
-         class Paint extends JPanel {
-             @Override
-             public void paint (Graphics g){
-                 Graphics2D g2d = (Graphics2D) g.create();
-                 super.paint(g2d);
-                 g.setColor(Color.red);
-                 g.drawOval(Integer.parseInt(locationX.getText()), Integer.parseInt(locationY.getText()), Integer.parseInt(radius.getText()), Integer.parseInt(radius.getText()));
-
+    private class MyCanvas extends Canvas {
+        //重写paint以绘制图形
+        @Override
+        public void paint(Graphics g) {
+            //绘制圆
+            if(shape.equals(OVAL_SHAPE)){
+                g.setColor(Color.red);
+                g.drawOval(Integer.parseInt(locationX.getText()),Integer.parseInt(locationY.getText()),Integer.parseInt(radius.getText()),Integer.parseInt(radius.getText()));
             }
         }
     }
