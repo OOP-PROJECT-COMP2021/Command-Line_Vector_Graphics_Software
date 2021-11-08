2b1ec5dbbce550419b45ffcb8c0242dcabf2f01e
Mike_Zhang <zhangwengyu999@gmail.com>
2021-11-08 17:09:52 +0800

    test

--- ClevisProjectGroup1/src/hk/edu/polyu/comp/comp2021/clevis/model/Clevis.java
+++ ClevisProjectGroup1/src/hk/edu/polyu/comp/comp2021/clevis/model/Clevis.java
@@ -3,6 +3,7 @@ package hk.edu.polyu.comp.comp2021.clevis.model;
 import java.util.*;
 
 /**Clevis Class*/
+
 public class Clevis {
     /**Clevis Constructor with empty body*/
     public Clevis(){}
