bee9e92f45cc17c373116e9e86932ba1d4bdcc92
Mike_Zhang <zhangwengyu999@gmail.com>
2021-11-08 17:21:30 +0800

    improve the code inspection in bounding box

--- ClevisProjectGroup1/src/hk/edu/polyu/comp/comp2021/clevis/model/BoundingBox.java
+++ ClevisProjectGroup1/src/hk/edu/polyu/comp/comp2021/clevis/model/BoundingBox.java
@@ -1,10 +1,15 @@
 package hk.edu.polyu.comp.comp2021.clevis.model;
+/** bounding box extends rectangle*/
 class BoundingBox extends Rectangle {
 
+    /** Bounding box constructor
+     * @param inShape : shape*/
     BoundingBox(Shape inShape) {
         super("Bounded " + inShape.getName(), inShape.getLeftBounding(), inShape.getTopBounding(), inShape.getRightBounding() - inShape.getLeftBounding(), inShape.getTopBounding() - inShape.getBottomBounding());
     }
 
+    /** get bounding box
+     * @return String of information of the bounding box*/
     public String getBoundingBox() {
         return (this.getLeftBounding()+" "+this.getTopBounding()+" "+ (this.getRightBounding()-this.getLeftBounding())+" "+(this.getTopBounding()-this.getBottomBounding()));
     }
